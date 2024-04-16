0ea9c414b8724557f08027839f6941c146a8940b
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-04-16 19:47:06 +0530

    feat: handle cors

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+var cors = require("cors");
+
 const apyV1 = require("./routers/apy.router.v1");
 const poolV1 = require("./routers/pool.router.v1");
 const { parseAddress } = require("./middlewares/parse-address.middleware");
+
 const app = express();
+const port = process.env.PORT || 8080;
 
-const port = 3000;
-
+app.use(cors());
 app.use(express.json());
+
 app.use("/v1/apy", parseAddress, apyV1);
 app.use("/v1/pool", parseAddress, poolV1);
 
